4453ccd86917ecfc3def5fa06b5e506d225d63a2
hajikazuo <hajikazuo@outlook.com>
2025-04-23 11:14:39 -0300

    Add project files.

---
+++ Recipient/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine("[*] Aguardando mensagens.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Recebido: {message}");
+};
+channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine("Pressione <ENTER> para sair.");
+Console.ReadLine();
---
+++ Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine("Digite sua mensagem e aperte <ENTER>");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "")
+        break;
+
+    var body = System.Text.Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: "",
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($"[x] Enviado: {message}");
+}
